c5e461f8ce6df64669b2bbec5277a787cc8828c4
PhWoich <pedrohenriquewoich@gmail.com>
2024-10-16 07:58:04 -0300

    Timer como vida implementado
    Implementado:
    - Timer em contagem regressiva
    - Função de dano que reduz do timer
    - Função de restauração que devolver o timer ao seu valor original
    - Game Over quando o timer zera
    - Reiniciar o jogo em game over ao apertar R

---
+++ ReencarnationsGuide/scenes/Game Manager/game_manager.gd
@@ -0,0 +1,53 @@
+extends Node
+
+var timer_start_value:float = 3.0
+var timer_cont:float = 0.0
+var timer_int:int = 0
+
+@onready var timer_view:Label = $"Timer View"
+@onready var game_over_view: Label = $"Game Over View"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	game_over_view.text = ""
+	timer_cont = timer_start_value
+	timer_int = timer_start_value
+	timer_view.text = str(timer_int)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if !game_over():
+		game_over_view.text = ""
+		timer_cont = timer_cont-delta
+		timer_int = timer_cont
+		timer_view.text = str(timer_int)
+	
+	if Input.is_action_just_released("restart_game") and game_over():
+		game_restart()
+	elif Input.is_action_just_released("damage"):
+		# Quando aperta 1 no teclado para testar
+		damage(10)
+	elif Input.is_action_just_released("restore"):
+		# Quando aperta 2 no teclado para testar
+		restore_all()
+
+func damage(damage_taken: int) -> void:
+	timer_cont = timer_cont-damage_taken
+	timer_int = timer_int-damage_taken
+
+
+func restore_all() -> void:
+	timer_cont = timer_start_value
+	timer_int = timer_start_value
+
+
+func game_over() -> bool:
+	if timer_int <= 0:
+		game_over_view.text = "GAME OVER!\nPress R to restart the game."
+		return true
+	else:
+		return false
+
+func game_restart() -> void:
+	get_tree().reload_current_scene()
